a0e2ccd8652e235a0dbaaf4ed39782aff0a2f225
Ned Grady <gradyned@gmail.com>
2023-02-20 16:45:30 +0000

    updated fizzbuzzify
    set up loop for 1 to 100 and update fizzbuzzify with fizz buzz and fizzbuzz

--- ConsoleApp1/Program.vb
+++ ConsoleApp1/Program.vb
@@ -2,11 +2,27 @@ Imports System
 
 Public Module Program
     Sub Main(args As String())
+        Dim i As Integer
 
+        For i = 1 To 100
+
+            Console.WriteLine(FizzBuzzify(i))
+
+        Next i
     End Sub
 
     Public Function FizzBuzzify(ByVal number As Integer)
-        Return 0
+        If number = 3 Then
+            Return "Fizz"
+
+        ElseIf number = 5 Then
+            Return "Buzz"
+
+        ElseIf number = 15 Then
+            Return "FizzBuzz"
+        End If
+
+        Return number
     End Function
 
 End Module
--- TestProject1/FizzBuzzTests.vb
+++ TestProject1/FizzBuzzTests.vb
@@ -8,11 +8,34 @@ Namespace TestProject1
 
         <Test>
         Public Sub OneReturnsOne()
-            Dim fizzBuzzResult As Integer = FizzBuzzify(1)
+            Dim fizzBuzzResult As String = FizzBuzzify(1)
 
-            Assert.AreEqual(fizzBuzzResult, 1)
+            Assert.AreEqual("1", fizzBuzzResult)
         End Sub
 
+        <Test>
+        Public Sub TwoReturnsTwo()
+            Dim fizzBuzzResult As String = FizzBuzzify(2)
+            Assert.AreEqual("2", fizzBuzzResult)
+        End Sub
+
+        <Test>
+        Public Sub ThreeIsFizz()
+            Dim fizzBuzzResult As String = FizzBuzzify(3)
+            Assert.AreEqual("Fizz", fizzBuzzResult)
+        End Sub
+
+        <Test>
+        Public Sub FiveisBuzz()
+            Dim fizzBuzzResult As String = FizzBuzzify(5)
+            Assert.AreEqual("Buzz", fizzBuzzResult)
+        End Sub
+
+        <Test>
+        Public Sub FifteenisFizzBuzz()
+            Dim fizzBuzzResult As String = FizzBuzzify(15)
+            Assert.AreEqual("FizzBuzz", fizzBuzzResult)
+        End Sub
     End Class
 
 End Namespace
